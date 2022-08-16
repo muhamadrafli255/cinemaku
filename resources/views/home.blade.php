d1df6aa63e36d1f706a959425c04428138a5d015
muhamadrafli255 <muhamad.rafli5255@gmail.com>
2022-08-17 01:17:01 +0700

    Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/mystyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>CINEMAKU | Home</title>
+</head>
+
+<body>
+
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none; fill: white;">
+        <symbol id="bootstrap" viewBox="0 0 118 94">
+          <title>Bootstrap</title>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
+        </symbol>
+        <symbol id="facebook" viewBox="0 0 16 16">
+          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+        </symbol>
+        <symbol id="instagram" viewBox="0 0 16 16">
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
+        </symbol>
+        <symbol id="twitter" viewBox="0 0 16 16">
+          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+        </symbol>
+      </svg>
+
+    {{-- Navbar --}}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-utama">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">CINEMAKU</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                </ul>
+                <span class="m-2">
+                    <button class="btn btn-outline-light" type="submit">Login</button>
+                </span>
+                <button class="btn btn-light" type="submit">Daftar</button>
+            </div>
+        </div>
+    </nav>
+    {{-- End Navbar --}}
+
+    {{-- Hero Section --}}
+    <section>
+        <div class="container mt-xl-10 mt-8">
+            <div class="row">
+                <div class="col-xl-6 col-sm-12 mt-5">
+                    <h2 class="fw-bold text-utama">Selamat Datang Dicinemaku</h2>
+                    <p class="fs-5">Booking tiket nonton film tanpa harus mengantri<br>Hanya dengan membuka handphone
+                        anda</p>
+                    <a href="" class="btn btn-primary bg-utama text-white">Booking Sekarang</a>
+                </div>
+                <div class="col-xl-5 col-sm-12 ma-5 mt-md-5 mb-md-5 mt-xl-0">
+                    <img src="/img/hero.png" alt="heroicon" width="640" height="470">
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- End Hero Section --}}
+
+    {{-- Superiority Section --}}
+<section>
+    <div class="container mt-6 mt-sm-3 mb-10">
+        <h2>Keunggulan</h2>
+        <div class="py-1 px-1 bg-utama mb-5"></div>
+        <div class="d-flex flex-xl-row flex-sm-column justify-content-center">
+            <div class="row">
+                <div class="col-sm-12 col-lg-4">
+            <div class="card m-xl-4 bg-utama" style="width: 15rem;">
+                <div class="card-body d-flex justify-content-center">
+                    <span class="text-white text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                          </svg>
+                          <h4 class="mt-4 fw-bold">Efisien</h4>
+                          <p class="fs-6">Pemesanan tiket jadi lebih cepat karena dipesan secara online</p>
+                    </span>
+                </div>
+            </div>
+            </div>
+            <div class="col-sm-12 col-lg-4">
+            <div class="card m-xl-4 bg-third" style="width: 15rem;">
+                <div class="card-body d-flex justify-content-center">
+                    <span class="text-white text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                          </svg>
+                          <h4 class="mt-4 fw-bold">Hemat Waktu</h4>
+                          <p class="fs-6">Pemesanan tidak harus mengantri ini bisa menghemat waktu anda</p>
+                    </span>
+                </div>
+            </div>
+            </div>
+            <div class="col-sm-12 col-lg-4">
+            <div class="card m-4 bg-utama" style="width: 15rem;">
+                <div class="card-body d-flex justify-content-center">
+                    <span class="text-white text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
+                          </svg>
+                          <h4 class="mt-4 fw-bold">Murah</h4>
+                          <p class="fs-6">Meskipun pemesanan online harga tiket tetap terjangkau</p>
+                    </span>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+    {{-- End Superiority Section --}}
+
+    {{-- New Movies Section --}}
+        <section>
+            <div class="container mb-10">
+                <h2>Film Terbaru</h2>
+                <div class="px-1 py-1 bg-utama mb-2"></div>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://cdn.cgv.id/uploads/movie/compressed/22015200.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title fw-bold">Mencuri Raden Saleh</h5>
+                              <span class="text-success d-flex flex-row mt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
+                                  </svg>
+                                  <p class="mt-1 m-2">Tersedia 20 Tiket Lagi</p>
+                              </span>
+                              <span class="text-danger d-flex flex-row mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                  </svg>
+                                  <p class="mt-1 m-2">Ditonton 1 Juta Orang</p>
+                              </span>
+                              <a href="#" class="btn btn-primary bg-utama">Pesan Tiket</a>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://cdn.cgv.id/uploads/movie/compressed/22014400.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">Bullet Train</h5>
+                                <span class="text-success d-flex flex-row mt-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Tersedia 155 Tiket Lagi</p>
+                                </span>
+                                <span class="text-danger d-flex flex-row mb-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Ditonton 30 Juta Orang</p>
+                                </span>
+                                <a href="#" class="btn btn-primary bg-utama">Pesan Tiket</a>
+                              </div>
+                          </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://cdn.cgv.id/uploads/movie/compressed/22013400.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">Pengabdi Setan 2</h5>
+                                <span class="text-success d-flex flex-row mt-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Tersedia 35 Tiket Lagi</p>
+                                </span>
+                                <span class="text-danger d-flex flex-row mb-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Ditonton 2 Juta Orang</p>
+                                </span>
+                                <a href="#" class="btn btn-primary bg-utama">Pesan Tiket</a>
+                              </div>
+                          </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://cdn.cgv.id/uploads/movie/compressed/22013200.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">Superpets</h5>
+                                <span class="text-success d-flex flex-row mt-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Tersedia 250 Tiket Lagi</p>
+                                </span>
+                                <span class="text-danger d-flex flex-row mb-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <p class="mt-1 m-2">Ditonton 7 Juta Orang</p>
+                                </span>
+                                <a href="#" class="btn btn-primary bg-utama">Pesan Tiket</a>
+                              </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    {{-- End New Movies --}}
+
+    {{-- Footer --}}
+    <div class="bg-utama py-2">
+    <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-5">
+        <div class="col-md-4 d-flex align-items-center">
+          <span class="mb-3 mb-md-0 text-white">© 2022 Cinemaku, Inc</span>
+        </div>
+    
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex text-white">
+          <li class="ms-3"><a class="text-white" href="#"><svg class="bi" width="24" height="24" style="fill: white;"><use xlink:href="#twitter"></use></svg></a></li>
+          <li class="ms-3"><a class="text-white" href="#"><svg class="bi" width="24" height="24" style="fill: white;"><use xlink:href="#instagram"></use></svg></a></li>
+          <li class="ms-3"><a class="text-white" href="#"><svg class="bi" width="24" height="24" style="fill: white;"><use xlink:href="#facebook"></use></svg></a></li>
+        </ul>
+      </footer>
+    </div>
+</div>
+    {{-- End Footer --}}
+
+</body>
+<script src="/js/bootstrap.min.js"></script>
+
+</html>
